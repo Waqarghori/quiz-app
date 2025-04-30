c04218d8d68bc0e95b8906a5a7d10c1036610fa5
Waqarghori <waqar.iqbal789@gmail.com>
2025-04-30 10:31:25 +0500

    upd

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ function startTimer(){
     if (sec > 59) {
         min++
         sec = 0;
-    } if (min >= 10) {
+    } if (min >= 3) {
         endTimmer()
     }
     getSec.innerText = sec
